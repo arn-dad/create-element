b576a3a15821fab5cc748b6b325f87b3254a8c5c
Arnold <dadayanaa@yahoo.com>
2019-11-02 06:35:26 -0700

    inital files

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/**
+ * Imlementetion of createElement function
+ * Description
+ *
+ * function must recive three arguments
+ * 1. element type ( 'div', 'span', 'button' )
+ * 2. element properties/attributes ( className, onclick, herf, style ) @NOTE it can be an object to hold key-value pair
+ * 3. element child @NOTE appendChild and/or innerHTML can be use
+ *
+ * @NOTE child can be string or another result of createElement function (Node), or for multiple elements it can be an array
+ */
+
+/**
+ * Imlementetion of render function
+ *  Description
+ *
+ * function must recive two arguments
+ * 1. Destination where created element should be rendered
+ * 2. Target element which will insert in to DOM
+ *
+ * @NOTE The destination can be div with some id usually it called root or app
+ * @NOTE Target can be DOM element or a function
+ */
+
+/**
+ * @TODO Create Card in example.html file using createElement function
+ * @TODO Create List of Cards using createElement function
+ * @TODO Render list of Cards in to DOM
+ *
+ * happy hacking ;)
+ */
